211b57c867e88ea7c15f4ce36190d8af30dfa540
alvaroswdn <39083254+alvaroswdn@users.noreply.github.com>
2025-03-04 15:41:12 +0700

    Cleanup authentication and validation system

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,23 +2,44 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import * as v from 'valibot'
 
 import { createClient } from '@/utils/supabase/server'
 
-export async function login(formData: FormData) {
+const LoginSchema = v.object({
+  email: v.pipe(
+    v.string('The email you entered is not a string'),
+    v.nonEmpty('The email field is missing from the form'),
+    v.email('The email you entered is not a valid email'),
+  ),
+  password: v.pipe(
+    v.string('The password you entered is not a string'),
+    v.nonEmpty('The password field is missing from the form'),
+    v.minLength(8, 'The password you entered is too short'),
+  ),
+})
+
+export type LoginForm = {
+  data: v.InferOutput<typeof LoginSchema>
+  errors: string[]
+}
+
+export async function login(prevState: { message: string }, formData: FormData) {
   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+  const parsing = v.safeParse(LoginSchema, {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if (!parsing.success) {
+    return { message: parsing.issues[0].message }
   }
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword(parsing.output)
 
   if (error) {
-    redirect('/error')
+    return { message: error.message }
   }
 
   revalidatePath('/', 'layout')
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,21 @@
 'use client'
 
+import { useProgress } from '@bprogress/next'
+import { useActionState, useEffect } from 'react'
 import { login } from './actions'
 
 export default function LoginPage() {
+  const progress = useProgress()
+  const [state, formAction, pending] = useActionState(login, { message: '' })
+
+  useEffect(() => {
+    if (pending) {
+      progress.start()
+    } else {
+      progress.stop()
+    }
+  }, [pending, progress])
+
   return (
     <main className="m-auto flex h-screen max-w-5xl flex-col items-center justify-center gap-12 p-4">
       <div className="text-center text-neutral-900">
@@ -10,33 +23,42 @@ export default function LoginPage() {
         <h1 className="text-2xl font-bold">IV Waste Management System</h1>
         <p className="text-secondary">Log in to your account to continue</p>
       </div>
-      <form action={login} className="flex flex-col items-center gap-4">
-        <div className="relative">
+      <form
+        onSubmit={() => progress.start()}
+        action={formAction}
+        className="flex flex-col items-center gap-4"
+      >
+        <h2>{state.message}</h2>
+        <div className="bg-quinary text-secondary relative w-full rounded-full">
           <label
             htmlFor="email"
-            className="pointer-events-none absolute top-2 left-6 text-sm font-semibold"
+            className="pointer-events-none absolute top-2 left-8 text-sm font-semibold"
           >
             Email
           </label>
           <input
             type="email"
             name="email"
-            className="bg-quinary text-secondary rounded-full border-none px-5 pt-8 pb-2"
+            className="w-full rounded-full px-8 pt-7 pb-2"
+            required
           />
         </div>
-        <div className="relative">
-          <label htmlFor="email" className="absolute top-2 left-6 text-sm font-semibold">
+        <div className="bg-quinary text-secondary relative w-full rounded-full">
+          <label htmlFor="email" className="absolute top-2 left-8 text-sm font-semibold">
             Password
           </label>
           <input
             type="password"
             name="password"
-            className="bg-quinary text-secondary rounded-full border-none px-5 pt-8 pb-2"
+            className="w-full rounded-full px-8 pt-7 pb-2"
+            minLength={8}
+            required
           />
         </div>
         <button
           type="submit"
-          className="bg-quinary w-fit cursor-pointer rounded-full px-5 pt-6 pb-2"
+          disabled={pending}
+          className="bg-quinary w-fit cursor-pointer rounded-full px-12 py-3 hover:bg-red-300"
         >
           Login
         </button>
